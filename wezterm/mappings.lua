ac0f3820fc7a3eda9755839993488ec9e0149f17
Joshua Manchester <joshuamanchester4@gmail.com>
2023-04-13 23:52:50 +0100

    Wezterm: Quick binding clash fix

--- wezterm/mappings.lua
+++ wezterm/mappings.lua
@@ -1,170 +1,166 @@
 local M = {}
 
-local wezterm = require("wezterm")
+local wezterm = require('wezterm')
 local act = wezterm.action
 
 M.load = function(o, util)
-    o.leader = { key = "Space", mods = "CTRL" }
-    o.keys = {
-        { key = "c", mods = "CTRL|SHIFT", action = act.CopyTo("Clipboard") },
-        { key = "v", mods = "CTRL|SHIFT", action = act.PasteFrom("Clipboard") },
-        { key = "p", mods = "ALT", action = act.PasteFrom("Clipboard") },
-        { key = "n", mods = "CTRL|SHIFT", action = act.SpawnWindow },
-        { key = "Enter", mods = "ALT", action = act.ToggleFullScreen },
-        { key = "=", mods = "ALT", action = act.IncreaseFontSize },
-        { key = "-", mods = "ALT", action = act.DecreaseFontSize },
-        { key = "Backspace", mods = "ALT", action = act.ResetFontSize },
-        { key = "n", mods = "ALT", action = act.SpawnTab("CurrentPaneDomain") },
-        { key = "w", mods = "ALT", action = act.CloseCurrentPane({ confirm = false }) },
-        { key = "l", mods = "CTRL|SHIFT", action = act.ShowDebugOverlay },
-        { key = "r", mods = "CTRL|SHIFT", action = act.ReloadConfiguration },
-        { key = "f", mods = "ALT", action = act.TogglePaneZoomState },
-        { key = "v", mods = "ALT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-        { key = "x", mods = "ALT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-        { key = "h", mods = "ALT", action = act.ActivatePaneDirection("Left") },
-        { key = "l", mods = "ALT", action = act.ActivatePaneDirection("Right") },
-        { key = "k", mods = "ALT", action = act.ActivatePaneDirection("Up") },
-        { key = "j", mods = "ALT", action = act.ActivatePaneDirection("Down") },
-        { key = "j", mods = "ALT|CTRL", action = act.ActivateTabRelative(-1) },
-        { key = "k", mods = "ALT|CTRL", action = act.ActivateTabRelative(1) },
-        { key = "o", mods = "ALT", action = act.OpenLinkAtMouseCursor },
-        { key = "y", mods = "ALT", action = act.ActivateCopyMode },
-        { key = "s", mods = "ALT", action = act.QuickSelect },
-        { key = "/", mods = "ALT", action = act.Search("CurrentSelectionOrEmptyString") },
-        { key = "o", mods = "ALT", action = act.ShowLauncher },
-        {
-            key = "l",
-            mods = "LEADER",
-            action = wezterm.action_callback(function(win, _)
-                util.toggle_theme_mode(win)
-            end),
-        },
-        {
-            key = "f",
-            mods = "LEADER",
-            action = wezterm.action_callback(function(win, _)
-                util.toggle_fetch_start(win)
-            end),
-        },
-    }
+  o.leader = { key = 'Space', mods = 'CTRL' }
+  o.keys = {
+    { key = 'c', mods = 'CTRL|SHIFT', action = act.CopyTo('Clipboard') },
+    { key = 'v', mods = 'CTRL|SHIFT', action = act.PasteFrom('Clipboard') },
+    { key = 'p', mods = 'CTRL|SHIFT', action = act.PasteFrom('Clipboard') },
+    { key = 'n', mods = 'CTRL|SHIFT', action = act.SpawnWindow },
+    { key = 'Enter', mods = 'ALT', action = act.ToggleFullScreen },
+    { key = '=', mods = 'CTRL|SHIFT', action = act.IncreaseFontSize },
+    { key = '-', mods = 'CTRL|SHIFT', action = act.DecreaseFontSize },
+    { key = 'Backspace', mods = 'CTRL|SHIFT', action = act.ResetFontSize },
+    { key = 'n', mods = 'CTRL|SHIFT', action = act.SpawnTab('CurrentPaneDomain') },
+    { key = 'w', mods = 'CTRL|SHIFT', action = act.CloseCurrentPane({ confirm = false }) },
+    { key = 'l', mods = 'CTRL|SHIFT', action = act.ShowDebugOverlay },
+    { key = 'r', mods = 'CTRL|SHIFT', action = act.ReloadConfiguration },
+    { key = 'f', mods = 'CTRL|SHIFT', action = act.TogglePaneZoomState },
+    { key = 'v', mods = 'CTRL|SHIFT', action = act.SplitHorizontal({ domain = 'CurrentPaneDomain' }) },
+    { key = 'x', mods = 'CTRL|SHIFT', action = act.SplitVertical({ domain = 'CurrentPaneDomain' }) },
+    { key = 'h', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection('Left') },
+    { key = 'l', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection('Right') },
+    { key = 'k', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection('Up') },
+    { key = 'j', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection('Down') },
+    { key = 'j', mods = 'ALT|CTRL', action = act.ActivateTabRelative(-1) },
+    { key = 'k', mods = 'ALT|CTRL', action = act.ActivateTabRelative(1) },
+    { key = 'o', mods = 'CTRL|SHIFT', action = act.OpenLinkAtMouseCursor },
+    { key = 'y', mods = 'CTRL|SHIFT', action = act.ActivateCopyMode },
+    { key = 's', mods = 'CTRL|SHIFT', action = act.QuickSelect },
+    { key = '/', mods = 'CTRL|SHIFT', action = act.Search('CurrentSelectionOrEmptyString') },
+    { key = 'o', mods = 'CTRL|SHIFT', action = act.ShowLauncher },
+    {
+      key = 'l',
+      mods = 'LEADER',
+      action = wezterm.action_callback(function(win, _) util.toggle_theme_mode(win) end),
+    },
+    {
+      key = 'f',
+      mods = 'LEADER',
+      action = wezterm.action_callback(function(win, _) util.toggle_fetch_start(win) end),
+    },
+  }
 
-    o.key_tables = {
-        copy_mode = {
-            { key = "Escape", mods = "", action = act.CopyMode("Close") },
-            { key = "h", mods = "", action = act.CopyMode("MoveLeft") },
-            { key = "l", mods = "", action = act.CopyMode("MoveRight") },
-            { key = "k", mods = "", action = act.CopyMode("MoveUp") },
-            { key = "j", mods = "", action = act.CopyMode("MoveDown") },
-            { key = "w", mods = "", action = act.CopyMode("MoveForwardWord") },
-            { key = "b", mods = "", action = act.CopyMode("MoveBackwardWord") },
-            {
-                key = "e",
-                mods = "",
-                action = act.Multiple({
-                    act.CopyMode("MoveRight"),
-                    act.CopyMode("MoveForwardWord"),
-                    act.CopyMode("MoveLeft"),
-                }),
-            },
-            { key = "0", mods = "", action = act.CopyMode("MoveToStartOfLine") },
-            { key = "\n", mods = "", action = act.CopyMode("MoveToStartOfNextLine") },
-            { key = "$", mods = "", action = act.CopyMode("MoveToEndOfLineContent") },
-            { key = "^", mods = "", action = act.CopyMode("MoveToStartOfLineContent") },
-            { key = "v", mods = "", action = act.CopyMode({ SetSelectionMode = "Cell" }) },
-            {
-                key = "v",
-                mods = "SHIFT",
-                action = act.Multiple({
-                    act.CopyMode("MoveToStartOfLineContent"),
-                    act.CopyMode({ SetSelectionMode = "Cell" }),
-                    act.CopyMode("MoveToEndOfLineContent"),
-                }),
-            },
-            {
-                key = "y",
-                mods = "",
-                action = act({
-                    Multiple = {
-                        act.CopyTo("Clipboard"),
-                        act.CopyMode("Close"),
-                    },
-                }),
-            },
-            {
-                key = "y",
-                mods = "SHIFT",
-                action = act({
-                    Multiple = {
-                        act.CopyMode({ SetSelectionMode = "Cell" }),
-                        act.CopyMode("MoveToEndOfLineContent"),
-                        act.CopyTo("Clipboard"),
-                        act.CopyMode("Close"),
-                    },
-                }),
-            },
-            { key = "g", mods = "", action = act.CopyMode("MoveToScrollbackTop") },
-            { key = "g", mods = "SHIFT", action = act.CopyMode("MoveToScrollbackBottom") },
-            { key = "u", mods = "", action = act.CopyMode("PageUp") },
-            { key = "d", mods = "", action = act.CopyMode("PageDown") },
-            { key = "/", mods = "", action = act.Search("CurrentSelectionOrEmptyString") },
-            {
-                key = "n",
-                mods = "",
-                action = act.Multiple({
-                    act.CopyMode("NextMatch"),
-                    act.CopyMode("ClearSelectionMode"),
-                }),
-            },
-            {
-                key = "n",
-                mods = "SHIFT",
-                action = act.Multiple({
-                    act.CopyMode("PriorMatch"),
-                    act.CopyMode("ClearSelectionMode"),
-                }),
-            },
-        },
-        search_mode = {
-            { key = "Escape", mods = "", action = act.CopyMode("Close") },
-            {
-                key = "Enter",
-                mods = "",
-                action = act.Multiple({
-                    act.CopyMode("ClearSelectionMode"),
-                    act.ActivateCopyMode,
-                }),
-            },
-            { key = "n", mods = "ALT|SHIFT|CTRL", action = act.CopyMode("PriorMatch") },
-            { key = "n", mods = "ALT|SHIFT", action = act.CopyMode("NextMatch") },
-            { key = "/", mods = "", action = act.CopyMode("ClearPattern") },
-        },
-    }
+  o.key_tables = {
+    copy_mode = {
+      { key = 'Escape', mods = '', action = act.CopyMode('Close') },
+      { key = 'h', mods = '', action = act.CopyMode('MoveLeft') },
+      { key = 'l', mods = '', action = act.CopyMode('MoveRight') },
+      { key = 'k', mods = '', action = act.CopyMode('MoveUp') },
+      { key = 'j', mods = '', action = act.CopyMode('MoveDown') },
+      { key = 'w', mods = '', action = act.CopyMode('MoveForwardWord') },
+      { key = 'b', mods = '', action = act.CopyMode('MoveBackwardWord') },
+      {
+        key = 'e',
+        mods = '',
+        action = act.Multiple({
+          act.CopyMode('MoveRight'),
+          act.CopyMode('MoveForwardWord'),
+          act.CopyMode('MoveLeft'),
+        }),
+      },
+      { key = '0', mods = '', action = act.CopyMode('MoveToStartOfLine') },
+      { key = '\n', mods = '', action = act.CopyMode('MoveToStartOfNextLine') },
+      { key = '$', mods = '', action = act.CopyMode('MoveToEndOfLineContent') },
+      { key = '^', mods = '', action = act.CopyMode('MoveToStartOfLineContent') },
+      { key = 'v', mods = '', action = act.CopyMode({ SetSelectionMode = 'Cell' }) },
+      {
+        key = 'v',
+        mods = 'SHIFT',
+        action = act.Multiple({
+          act.CopyMode('MoveToStartOfLineContent'),
+          act.CopyMode({ SetSelectionMode = 'Cell' }),
+          act.CopyMode('MoveToEndOfLineContent'),
+        }),
+      },
+      {
+        key = 'y',
+        mods = '',
+        action = act({
+          Multiple = {
+            act.CopyTo('Clipboard'),
+            act.CopyMode('Close'),
+          },
+        }),
+      },
+      {
+        key = 'y',
+        mods = 'SHIFT',
+        action = act({
+          Multiple = {
+            act.CopyMode({ SetSelectionMode = 'Cell' }),
+            act.CopyMode('MoveToEndOfLineContent'),
+            act.CopyTo('Clipboard'),
+            act.CopyMode('Close'),
+          },
+        }),
+      },
+      { key = 'g', mods = '', action = act.CopyMode('MoveToScrollbackTop') },
+      { key = 'g', mods = 'SHIFT', action = act.CopyMode('MoveToScrollbackBottom') },
+      { key = 'u', mods = '', action = act.CopyMode('PageUp') },
+      { key = 'd', mods = '', action = act.CopyMode('PageDown') },
+      { key = '/', mods = '', action = act.Search('CurrentSelectionOrEmptyString') },
+      {
+        key = 'n',
+        mods = '',
+        action = act.Multiple({
+          act.CopyMode('NextMatch'),
+          act.CopyMode('ClearSelectionMode'),
+        }),
+      },
+      {
+        key = 'n',
+        mods = 'SHIFT',
+        action = act.Multiple({
+          act.CopyMode('PriorMatch'),
+          act.CopyMode('ClearSelectionMode'),
+        }),
+      },
+    },
+    search_mode = {
+      { key = 'Escape', mods = '', action = act.CopyMode('Close') },
+      {
+        key = 'Enter',
+        mods = '',
+        action = act.Multiple({
+          act.CopyMode('ClearSelectionMode'),
+          act.ActivateCopyMode,
+        }),
+      },
+      { key = 'n', mods = 'ALT|SHIFT|CTRL', action = act.CopyMode('PriorMatch') },
+      { key = 'n', mods = 'ALT|SHIFT', action = act.CopyMode('NextMatch') },
+      { key = '/', mods = '', action = act.CopyMode('ClearPattern') },
+    },
+  }
 
-    o.mouse_bindings = {
-        {
-            event = { Down = { streak = 1, button = { WheelUp = 1 } } },
-            mods = "",
-            action = act.ScrollByCurrentEventWheelDelta,
-        },
-        {
-            event = { Down = { streak = 1, button = { WheelDown = 1 } } },
-            mods = "",
-            action = act.ScrollByCurrentEventWheelDelta,
-        },
-        {
-            event = { Up = { streak = 1, button = "Left" } },
-            mods = "CTRL",
-            action = "OpenLinkAtMouseCursor",
-        },
-    }
+  o.mouse_bindings = {
+    {
+      event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+      mods = '',
+      action = act.ScrollByCurrentEventWheelDelta,
+    },
+    {
+      event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+      mods = '',
+      action = act.ScrollByCurrentEventWheelDelta,
+    },
+    {
+      event = { Up = { streak = 1, button = 'Left' } },
+      mods = 'CTRL',
+      action = 'OpenLinkAtMouseCursor',
+    },
+  }
 
-    for i = 1, 9 do
-        table.insert(o.keys, {
-            key = tostring(i),
-            mods = "ALT",
-            action = act.ActivateTab(i - 1),
-        })
-    end
+  for i = 1, 9 do
+    table.insert(o.keys, {
+      key = tostring(i),
+      mods = 'ALT',
+      action = act.ActivateTab(i - 1),
+    })
+  end
 end
 
 return M
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,74 +1,74 @@
-local wezterm = require("wezterm")
-local util = require("util")
+local wezterm = require('wezterm')
+local util = require('util')
 
 local o = {}
 
 -- General
 if wezterm.GLOBAL.fetchstart == nil then
-    wezterm.GLOBAL.fetchstart = true
+  wezterm.GLOBAL.fetchstart = true
 end
-if util.has("win") then
-    if wezterm.GLOBAL.fetchstart then
-        o.default_prog = { "C:\\Users\\Joshua\\scoop\\shims\\pwsh.exe", "-nologo", "-noexit", "-command", "winfetch" }
-    else
-        o.default_prog = { "C:\\Users\\Joshua\\scoop\\shims\\pwsh.exe", "-nologo" }
-    end
+if util.has('win') then
+  if wezterm.GLOBAL.fetchstart then
+    o.default_prog = { 'C:\\Users\\Joshua\\scoop\\shims\\pwsh.exe', '-nologo', '-noexit', '-command', 'winfetch' }
+  else
+    o.default_prog = { 'C:\\Users\\Joshua\\scoop\\shims\\pwsh.exe', '-nologo' }
+  end
 end
 local wsl_domains = wezterm.default_wsl_domains()
 local primary_wsl_domain = nil
 for _, domain in ipairs(wsl_domains) do
-    if primary_wsl_domain == nil then
-        primary_wsl_domain = domain
-    end
-    domain.default_cwd = "~"
-    if wezterm.GLOBAL.fetchstart then
-        domain.default_prog = { "zsh", "-c", "neofetch && zsh -l" }
-    end
+  if primary_wsl_domain == nil then
+    primary_wsl_domain = domain
+  end
+  domain.default_cwd = '~'
+  if wezterm.GLOBAL.fetchstart then
+    domain.default_prog = { 'zsh', '-c', 'neofetch && zsh -l' }
+  end
 end
 o.wsl_domains = wsl_domains
 
 -- Visual
 o.font_size = 14
-o.font = wezterm.font("FiraCode NFM")
+o.font = wezterm.font('FiraCode NFM')
 o.custom_block_glyphs = false
 -- o.window_decorations = "NONE"
 o.initial_rows = 21
 o.initial_cols = 98
 o.launch_menu = {
-    {
-        label = "Neovim",
-        args = { "nvim" },
-    },
+  {
+    label = 'Neovim',
+    args = { 'nvim' },
+  },
 }
-if util.has("win") then
-    table.insert(o.launch_menu, {
-        label = "Neovim WSL",
-        domain = { DomainName = primary_wsl_domain.name },
-        cwd = "~",
-        args = { "zsh", "-l", "-c", "nvim" },
-    })
+if util.has('win') then
+  table.insert(o.launch_menu, {
+    label = 'Neovim WSL',
+    domain = { DomainName = primary_wsl_domain.name },
+    cwd = '~',
+    args = { 'zsh', '-l', '-c', 'nvim' },
+  })
 end
 o.inactive_pane_hsb = {
-    saturation = 1,
-    brightness = 1,
+  saturation = 1,
+  brightness = 1,
 }
 o.adjust_window_size_when_changing_font_size = false
 o.hide_tab_bar_if_only_one_tab = true
 
 -- Performance
-if util.has("win") then
-    o.max_fps = 165
+if util.has('win') then
+  o.max_fps = 165
 end
 o.status_update_interval = 1000
 
 -- Sunset/sunrise colorscheme
-util.colorscheme_init("AyuLight (Gogh)", "AyuMirage (Gogh)", 50.8229, -0.1363)
+util.colorscheme_init('AyuLight (Gogh)', 'AyuMirage (Gogh)', 50.8229, -0.1363)
 o.color_scheme = wezterm.GLOBAL.colorscheme
 
 -- Mappings
 o.disable_default_key_bindings = true
 o.disable_default_mouse_bindings = true
 o.use_dead_keys = false
-require("mappings").load(o, util)
+require('mappings').load(o, util)
 
 return o
