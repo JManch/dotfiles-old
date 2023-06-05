b2de494233336b859cd0d6e947c7127365313c9d
Joshua Manchester <joshuamanchester4@gmail.com>
2023-06-05 16:55:31 +0100

    Powershell: update aliases

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -3,19 +3,26 @@ Set-Alias -Name ls -Value lsd
 
 # ll alias
 function ll_func {
-	lsd -l;
+	lsd -la;
 }
 Set-Alias -Name ll -Value ll_func
 
 # Neovim nightly alias
-Set-Alias -Name nnvim -Value "C:\NVME Files\Personal\Programs\Neovim-nightly\bin\nvim.exe"
+Set-Alias -Name nvim -Value "C:\NVME Files\Personal\Programs\Neovim-nightly\bin\nvim.exe"
 
 # dotfiles alias
 function dotfiles_func {
     git --git-dir=$HOME\.config\.dotfiles --work-tree=$HOME\.config $args;
 }
 
-Set-Alias -Name dotfiles -Value dotfiles_func
+Set-Alias -Name dotfiles -Value dotfiles_func 
+
+function lazygit_dotfiles_func {
+    lazygit --git-dir=$HOME\.config\.dotfiles --work-tree=$HOME\.config;
+}
+
+# lazygit dotfiles alias
+Set-Alias -Name dotfileslazy -Value lazygit_dotfiles_func
 
 # Init starship
 Invoke-Expression (&starship init powershell)
